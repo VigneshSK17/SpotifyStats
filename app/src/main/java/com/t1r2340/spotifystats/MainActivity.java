9591bd88d3ed0d460e477dadb23818508ff9cce8
VigneshSK17 <vigsk17@gmail.com>
2024-03-01 18:41:53 -0500

    First commit

---
+++ app/src/main/java/com/t1r2340/spotifystats/MainActivity.java
@@ -0,0 +1,14 @@
+package com.t1r2340.spotifystats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
