a57d8d935a681ae533ce17cda755bfe26ec5e269
sajanbtom <sajanbtom@gmail.com>
2024-03-26 13:26:03 -0400

    Firebase sign in in progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+
 
     implementation("com.spotify.android:auth:2.1.1")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
--- app/src/main/java/com/t1r2340/spotifystats/MainActivity.java
+++ app/src/main/java/com/t1r2340/spotifystats/MainActivity.java
@@ -1,33 +1,39 @@
 package com.t1r2340.spotifystats;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationRequest;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
 import com.t1r2340.spotifystats.helpers.FailureCallback;
 import com.t1r2340.spotifystats.helpers.SpotifyApi;
-import com.t1r2340.spotifystats.models.api.SpotifyProfile;
 import com.t1r2340.spotifystats.models.api.TopArtists;
 import com.t1r2340.spotifystats.models.api.TopTracks;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements FailureCallback {
 
@@ -44,6 +50,15 @@ public class MainActivity extends AppCompatActivity implements FailureCallback {
   private TextView tokenTextView, codeTextView, profileTextView;
 
   private SpotifyApi spotifyApi;
+  private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+          new FirebaseAuthUIActivityResultContract(),
+          new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+            @Override
+            public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+              onSignInResult(result);
+            }
+          }
+  );
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +90,37 @@ public class MainActivity extends AppCompatActivity implements FailureCallback {
     });
 
   }
+  public void createSignInIntent() {
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
+
+    Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build();
+    signInLauncher.launch(signInIntent);
+  }
+  private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+    IdpResponse response = result.getIdpResponse();
+    if (result.getResultCode() == RESULT_OK) {
+      // Successfully signed in
+      FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+      getCode();
+    } else {
+      Toast.makeText(this, "Sign In Failed!", Toast.LENGTH_SHORT).show();
+    }
+  }
+  public void signOut() {
+    AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener(new OnCompleteListener<Void>() {
+              public void onComplete(@NonNull Task<Void> task) {
+                // ...
+              }
+            });
+  }
+
+
 
   /**
    * Get token from Spotify
