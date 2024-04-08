8047f244a567ff857101a42db2442a1101594fbd
Vignesh Suresh Kumar <55813127+VigneshSK17@users.noreply.github.com>
2024-04-07 22:32:52 -0400

    Revert "Added spotify api, firebase auth & firestore, and ui design (#12)"
    This reverts commit 6e8e7a1e58b81c9d50c8de736ab37804cfe0e330.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        manifestPlaceholders["redirectSchemeName"] = "spotifystats"
-        manifestPlaceholders["redirectHostName"] = "auth"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -34,9 +31,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -44,23 +38,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("spotify-app-remote-release-0.8.0.aar"))
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
-    implementation("androidx.navigation:navigation-fragment:2.7.6")
-    implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
-
-    implementation("com.spotify.android:auth:2.1.1")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
-
-    implementation("com.google.code.gson:gson:2.8.6")
-
 }
--- app/src/main/java/com/t1r2340/spotifystats/helpers/FailureCallback.java
+++
@@ -1,5 +0,0 @@
-package com.t1r2340.spotifystats.helpers;
-
-public interface FailureCallback {
-    void onFailure(Exception e);
-}
